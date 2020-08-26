f17768fe07c87052f41b6f94667131f52bc3a63b
Miguel Gavilán <miangame1@gmail.com>
2020-08-26 16:06:37 +0200

    Added createdAt to devit

---
+++ hooks/useDateTimeFormat.js
@@ -0,0 +1,17 @@
+import { DEFAULT_LANGUAGE } from "constants/locale"
+
+export default function useDateTimeFormat(timestamp) {
+  const date = new Date(timestamp)
+  const language = navigator.language || DEFAULT_LANGUAGE
+
+  const options = {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  }
+
+  return new Intl.DateTimeFormat(language, options).format(date)
+}
